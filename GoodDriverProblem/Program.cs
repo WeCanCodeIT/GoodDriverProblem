fa5f8c9a70b96f449a6061a584c8d51ce8c3a6cf
Your Name <you@example.com>
2017-08-22 09:26:32 -0400

    pushing up bin

--- GoodDriverProblem/Program.cs
+++ GoodDriverProblem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace GoodDriverProblem
     {
         static void Main(string[] args)
         {
+            StreamReader record = new StreamReader("DrivingRecord.txt");
+            Console.WriteLine(record.ReadToEnd());
 
         }
     }
